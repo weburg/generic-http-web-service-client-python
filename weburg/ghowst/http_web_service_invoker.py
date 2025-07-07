bcae326bae00a479a91a536ab208bbe01daec3dd
Wren Weburg <wren@weburg.com>
2025-07-06 18:54:04 -0700

    Consolidate duplicate code, argument processing more consistent and flexible, logging now instead of prints, rename entity to resource, more consistent and flexible return statements.

--- weburg/ghowst/http_web_service_invoker.py
+++ weburg/ghowst/http_web_service_invoker.py
@@ -1,5 +1,6 @@
 import _io
 import json
+import logging
 import urllib.parse
 from json import JSONDecodeError
 from types import SimpleNamespace
@@ -11,7 +12,7 @@ from weburg.ghowst.http_web_service_exception import HttpWebServiceException
 
 class HTTPWebServiceInvoker:
     @staticmethod
-    def __get_entity_name(name, verb):
+    def __get_resource_name(name, verb):
         return name[len(verb) + 1 : len(name)].lower()
 
     @staticmethod
@@ -56,148 +57,115 @@ class HTTPWebServiceInvoker:
                     return True
         return False
 
+    @staticmethod
+    def __handle_result(result):
+        if result.status_code >= 400 or result.status_code < 200:
+            raise HttpWebServiceException(result.status_code, result.headers["x-error-message"])
+        elif result.status_code >= 300 and result.status_code < 400:
+            raise HttpWebServiceException(result.status_code, result.headers["location"])
+
+        try:
+            return json.loads(result.text, object_hook=lambda d: SimpleNamespace(**d))
+        except JSONDecodeError:
+            return None
+
+    def __http_entity_from_arguments(self, arguments):
+        values = {}
+        files = {}
+
+        for name, value in arguments.items():
+            if not self.__has_user_properties(value):
+                if type(value) == _io.BufferedReader:
+                    files[self.__underbar_to_camel(name)] = value
+                else:
+                    values[self.__underbar_to_camel(name)] = value
+            else:
+                for property in vars(value):
+                    if type(value.__dict__[property]) == _io.BufferedReader:
+                        files[self.__underbar_to_camel(name) + '.' + self.__underbar_to_camel(property)] = (
+                            value.__dict__[property].name, value.__dict__[property]
+                        )
+                    elif not property.startswith("__"):
+                        values[self.__underbar_to_camel(name) + '.' + self.__underbar_to_camel(property)] = (
+                            value.__dict__[property]
+                        )
+
+        return values, files
+
     def invoke(self, method_name, arguments, base_url):
         if method_name.startswith("get"):
             verb = "get"
-            entity = self.__get_entity_name(method_name, verb)
+            resource = self.__get_resource_name(method_name, verb)
         elif method_name.startswith("create_or_replace"):
             verb = "create_or_replace"
-            entity = self.__get_entity_name(method_name, verb)
+            resource = self.__get_resource_name(method_name, verb)
         elif method_name.startswith("create"):
             verb = "create"
-            entity = self.__get_entity_name(method_name, verb)
+            resource = self.__get_resource_name(method_name, verb)
         elif method_name.startswith("update"):
             verb = "update"
-            entity = self.__get_entity_name(method_name, verb)
+            resource = self.__get_resource_name(method_name, verb)
         elif method_name.startswith("delete"):
             verb = "delete"
-            entity = self.__get_entity_name(method_name, verb)
+            resource = self.__get_resource_name(method_name, verb)
         else:
             parts = method_name.split('_')
 
             verb = parts[0].lower()
-            entity = self.__get_entity_name(method_name, verb)
+            resource = self.__get_resource_name(method_name, verb)
 
-        print(f"Verb: {verb}")
-        print(f"Entity: {entity}")
+        logging.info(f"Verb: {verb}")
+        logging.info(f"Resource: {resource}")
+
+        headers = {"accept": "application/json"}
 
         try:
             if verb == "get":
-                uri_str = base_url + '/' + entity + self.__generate_qs(arguments)
+                uri_str = base_url + '/' + resource + self.__generate_qs(arguments)
 
-                result = requests.get(uri_str, headers={"accept": "application/json"})
+                result = requests.get(uri_str, headers=headers)
 
-                if result.status_code >= 400 or result.status_code < 200:
-                    raise HttpWebServiceException(result.status_code, result.headers["x-error-message"])
-                elif result.status_code >= 300 and result.status_code < 400:
-                    raise HttpWebServiceException(result.status_code, result.headers["location"])
-
-                return json.loads(result.text, object_hook=lambda d: SimpleNamespace(**d))
+                return self.__handle_result(result)
             elif verb == "create":
-                uri_str = base_url + '/' + entity
+                uri_str = base_url + '/' + resource
 
-                files = {}
-                values = {}
+                values, files = self.__http_entity_from_arguments(arguments)
 
-                for name, value in arguments.items():
-                    for property in vars(value):
-                        if type(value.__dict__[property]) == _io.BufferedReader:
-                            # Field type is a File
-                            files[self.__underbar_to_camel(name) + '.' + self.__underbar_to_camel(property)] = (value.__dict__[property].name, value.__dict__[property])
-                        elif not property.startswith("__"):
-                            # Field type is normal / text
-                            values[self.__underbar_to_camel(name) + '.' + self.__underbar_to_camel(property)] = value.__dict__[property]
+                result = requests.post(uri_str, data=values, files=files, headers=headers)
 
-                result = requests.post(uri_str, files=files, data=values, headers={"accept": "application/json"})
-
-                if result.status_code >= 400 or result.status_code < 200:
-                    raise HttpWebServiceException(result.status_code, result.headers["x-error-message"])
-                elif result.status_code >= 300 and result.status_code < 400:
-                    raise HttpWebServiceException(result.status_code, result.headers["location"])
-
-                return json.loads(result.text, object_hook=lambda d: SimpleNamespace(**d))
+                return self.__handle_result(result)
             elif verb == "create_or_replace":
-                uri_str = base_url + '/' + entity
+                uri_str = base_url + '/' + resource
 
-                files = {}
-                values = {}
+                values, files = self.__http_entity_from_arguments(arguments)
 
-                for name, value in arguments.items():
-                    for property in vars(value):
-                        if type(value.__dict__[property]) == _io.BufferedReader:
-                            # Field type is a File
-                            files[self.__underbar_to_camel(name) + '.' + self.__underbar_to_camel(property)] = (value.__dict__[property].name, value.__dict__[property])
-                        elif not property.startswith("__"):
-                            # Field type is normal / text
-                            values[self.__underbar_to_camel(name) + '.' + self.__underbar_to_camel(property)] = value.__dict__[property]
+                result = requests.put(uri_str, data=values, files=files, headers=headers)
 
-                result = requests.put(uri_str, files=files, data=values, headers={"accept": "application/json"})
-
-                if result.status_code >= 400 or result.status_code < 200:
-                    raise HttpWebServiceException(result.status_code, result.headers["x-error-message"])
-                elif result.status_code >= 300 and result.status_code < 400:
-                    raise HttpWebServiceException(result.status_code, result.headers["location"])
-
-                return json.loads(result.text, object_hook=lambda d: SimpleNamespace(**d))
+                return self.__handle_result(result)
             elif verb == "update":
-                uri_str = base_url + '/' + entity
+                uri_str = base_url + '/' + resource
 
-                files = {}
-                values = {}
+                values, files = self.__http_entity_from_arguments(arguments)
 
-                for name, value in arguments.items():
-                    for property in vars(value):
-                        if type(value.__dict__[property]) == _io.BufferedReader:
-                            # Field type is a File
-                            files[self.__underbar_to_camel(name) + '.' + self.__underbar_to_camel(property)] = (value.__dict__[property].name, value.__dict__[property])
-                        elif not property.startswith("__"):
-                            # Field type is normal / text
-                            values[self.__underbar_to_camel(name) + '.' + self.__underbar_to_camel(property)] = value.__dict__[property]
+                result = requests.patch(uri_str, data=values, files=files, headers=headers)
 
-                result = requests.patch(uri_str, files=files, data=values, headers={"accept": "application/json"})
-
-                if result.status_code >= 400 or result.status_code < 200:
-                    raise HttpWebServiceException(result.status_code, result.headers["x-error-message"])
-                elif result.status_code >= 300 and result.status_code < 400:
-                    raise HttpWebServiceException(result.status_code, result.headers["location"])
-
-                return None
+                return self.__handle_result(result)
             elif verb == "delete":
-                uri_str = base_url + '/' + entity + self.__generate_qs(arguments)
+                uri_str = base_url + '/' + resource + self.__generate_qs(arguments)
 
-                result = requests.delete(uri_str, headers={"accept": "application/json"})
+                result = requests.delete(uri_str, headers=headers)
 
-                if result.status_code >= 400 or result.status_code < 200:
-                    raise HttpWebServiceException(result.status_code, result.headers["x-error-message"])
-                elif result.status_code >= 300 and result.status_code < 400:
-                    raise HttpWebServiceException(result.status_code, result.headers["location"])
-
-                return None
+                return self.__handle_result(result)
             else:
                 # POST to a custom verb resource
 
-                uri_str = base_url + '/' + entity + '/' + verb
+                uri_str = base_url + '/' + resource + '/' + verb
 
-                values = {}
+                values, files = self.__http_entity_from_arguments(arguments)
 
-                for name, value in arguments.items():
-                    if not self.__has_user_properties(value):
-                        values[self.__underbar_to_camel(name)] = value
-                    else:
-                        for property in vars(value):
-                            values[self.__underbar_to_camel(name) + '.' + self.__underbar_to_camel(property)] = value.__dict__[property]
+                result = requests.post(uri_str, data=values, files=files, headers=headers)
 
-                result = requests.post(uri_str, data=values, headers={"accept": "application/json"})
-
-                if result.status_code >= 400 or result.status_code < 200:
-                    raise HttpWebServiceException(result.status_code, result.headers["x-error-message"])
-                elif result.status_code >= 300 and result.status_code < 400:
-                    raise HttpWebServiceException(result.status_code, result.headers["location"])
-
-                try:
-                    return json.loads(result.text, object_hook=lambda d: SimpleNamespace(**d))
-                except JSONDecodeError:
-                    return None
+                return self.__handle_result(result)
         except HttpWebServiceException as e:
             raise e
         except Exception as e:
